fb324ad11fd65051135ad8036d79e3e1458605f7
Vladimir Basko <1989bvg@gmail.com>
2023-05-28 18:38:31 +0200

    transfered getCommentsForPost endpoint to typeOrm

--- src/comments/infrastructure/query-comments.repository.ts
+++ src/comments/infrastructure/query-comments.repository.ts
@@ -10,6 +10,7 @@ import {
 import {
   countSkipValue,
   getCommonInfoForQueryAllRequests,
+  getDbSortDirection,
 } from '../../common/utils';
 import {
   mapDbCommentToBloggerCommentOutputModel,
@@ -20,6 +21,13 @@ import { CommentsQueryParamsDto } from '../api/dto/comments-query-params.dto';
 import { QueryLikesRepository } from '../../likes/infrastructure/query-likes.repository';
 import { DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE } from '../../common/constants';
 import { CommentEntity } from '../entities/db-entities/comment.entity';
+
+interface ICommentsDataByQueryParams {
+  comments: CommentEntity[];
+  totalCount: number;
+  pageSize: number;
+  pageNumber: number;
+}
 
 @Injectable()
 export class QueryCommentsRepository {
@@ -136,58 +144,32 @@ export class QueryCommentsRepository {
     queryParams: CommentsQueryParamsDto,
     isBanned?: boolean,
     postId?: string,
-  ): Promise<any> {
+  ): Promise<ICommentsDataByQueryParams> {
     const {
       sortBy = CommentSortByField.createdAt,
       sortDirection = SortDirection.desc,
       pageNumber = DEFAULT_PAGE_NUMBER,
       pageSize = DEFAULT_PAGE_SIZE,
     } = queryParams;
-    const offset = countSkipValue(pageNumber, pageSize);
-    const postIdCondition = postId
-      ? `WHERE "comment"."postId" = '${postId}'`
-      : '';
-    const isBannedCondition =
-      typeof isBanned === 'boolean'
-        ? `AND "user"."isBanned" = ${isBanned}`
-        : '';
-    const countResult = await this.dataSource.query(
-      ` SELECT COUNT(*)
-        FROM "comment"
-          LEFT JOIN "user" ON "user"."id" = "comment"."authorId"
-          ${postIdCondition}
-          ${isBannedCondition}
-      `,
-    );
-    const totalCount = countResult[0].count;
-    const comments = await this.dataSource.query(
-      ` SELECT
-          "comment".*,
-          "user"."login" as "userLogin"
-        FROM "comment"
-          LEFT JOIN "user" ON "user"."id" = "comment"."authorId"
-          ${postIdCondition}
-          ${isBannedCondition}
-        ORDER BY
-          CASE
-            WHEN $1 = 'asc' THEN
-              CASE
-                WHEN $2 = '${CommentSortByField.content}' THEN "comment"."content"
-                ELSE "comment"."createdAt"::varchar
-              END
-            END ASC,
-          CASE
-            WHEN $1 = 'desc' THEN
-              CASE
-                WHEN $2 = '${CommentSortByField.content}' THEN "comment"."content"
-                ELSE "comment"."createdAt"::varchar
-              END
-            END DESC
-          OFFSET $3 LIMIT $4
-      
-      `,
-      [sortDirection, sortBy, offset, pageSize],
-    );
+    const skip = countSkipValue(pageNumber, pageSize);
+    const dbSortDirection = getDbSortDirection(sortDirection);
+    const selectQueryBuilder = this.typeOrmCommentRepository
+      .createQueryBuilder('comment')
+      .innerJoinAndSelect('comment.user', 'user');
+
+    if (isBanned) {
+      selectQueryBuilder.where('user.isBanned = :isBanned', { isBanned });
+    }
+    if (postId) {
+      selectQueryBuilder.andWhere('comment.postId = :postId', { postId });
+    }
+
+    const totalCount = await selectQueryBuilder.getCount();
+    const comments = await selectQueryBuilder
+      .orderBy(`comment.${sortBy}`, dbSortDirection)
+      .skip(skip)
+      .take(pageSize)
+      .getMany();
 
     return {
       comments,

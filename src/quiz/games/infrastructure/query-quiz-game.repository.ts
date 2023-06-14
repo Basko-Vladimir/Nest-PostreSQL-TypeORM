2df5608a47a8f1db373664e9b12ec07c25edeb80
Vladimir Basko <1989bvg@gmail.com>
2023-06-14 14:44:07 +0200

    fixed typeOrm request to db for getCurrentGame

--- src/quiz/games/infrastructure/query-quiz-game.repository.ts
+++ src/quiz/games/infrastructure/query-quiz-game.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, SelectQueryBuilder } from 'typeorm';
+import { Brackets, Repository, SelectQueryBuilder } from 'typeorm';
 import { QuizGameEntity } from '../entities/quiz-game.entity';
 import { mapQuizGameEntityToQuizGameOutputModel } from '../mappers/quiz-game.mapper';
 import { IQuizGameOutputModel } from '../api/dto/quiz-game-output-models.dto';
@@ -25,13 +25,24 @@ export class QueryQuizGameRepository {
   async getCurrentGame(userId: string): Promise<IQuizGameOutputModel> {
     const selectQueryBuilder = this.createSelectQueryBuilder();
     const currentGame = await selectQueryBuilder
-      .where('game.firstPlayerId = :userId OR game.secondPlayerId = :userId', {
-        userId,
-      })
-      .andWhere('game.status = :activeStatus OR game.status = :pendingStatus', {
-        activeStatus: QuizGameStatus.ACTIVE,
-        pendingStatus: QuizGameStatus.PENDING_SECOND_PLAYER,
-      })
+      .where(
+        new Brackets((qb) => {
+          qb.where('game.firstPlayerId = :firstPlayerId', {
+            firstPlayerId: userId,
+          }).orWhere('game.secondPlayerId = :secondPlayerId', {
+            secondPlayerId: userId,
+          });
+        }),
+      )
+      .andWhere(
+        new Brackets((qb) => {
+          qb.where('game.status = :activeStatus', {
+            activeStatus: QuizGameStatus.ACTIVE,
+          }).orWhere('game.status = :pendingStatus', {
+            pendingStatus: QuizGameStatus.PENDING_SECOND_PLAYER,
+          });
+        }),
+      )
       .getOne();
 
     return currentGame && mapQuizGameEntityToQuizGameOutputModel(currentGame);

cae8017818433dd345de28ffacba5d82487018dd
Vladimir Basko <1989bvg@gmail.com>
2023-08-13 17:19:23 +0200

    fixed some tests

--- src/files-uploading/mappers/file-uploading-mappers.ts
+++ src/files-uploading/mappers/file-uploading-mappers.ts
@@ -5,6 +5,8 @@ import {
   IPostFileUploadingOutputModelDto,
 } from '../api/dto/file-uploading-output-models.dto';
 
+const BASE_URL = 'https://devbucket1.storage.yandexcloud.net/';
+
 export const mapBlogFileUploadingEntityToBlogFileUploadingOutputModel = (
   fileUploadings: FileUploadingEntity[],
 ): IBlogFileUploadingOutputModelDto => {
@@ -16,17 +18,19 @@ export const mapBlogFileUploadingEntityToBlogFileUploadingOutputModel = (
   );
 
   return {
-    wallpaper: {
-      url: wallpaper?.url,
-      width: wallpaper?.width,
-      height: wallpaper?.height,
-      fileSize: wallpaper?.size,
-    },
+    wallpaper: wallpaper
+      ? {
+          url: `${BASE_URL}${wallpaper.url}`,
+          width: wallpaper.width,
+          height: wallpaper.height,
+          fileSize: wallpaper.size,
+        }
+      : null,
     main: mainFiles.map((item) => ({
-      url: item?.url,
-      width: item?.width,
-      height: item?.height,
-      fileSize: item?.size,
+      url: item && `${BASE_URL}${item.url}`,
+      width: item.width,
+      height: item.height,
+      fileSize: item.size,
     })),
   };
 };
